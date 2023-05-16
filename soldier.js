02699afae7a1f349b47e909d678c2067a6652b21
BesaMugi <mugiwarabesa@mail.ru>
2023-05-16 22:10:48 +0300

    доделал soldier

--- soldier.js
+++ soldier.js
@@ -1,5 +1,5 @@
 const soldier = {
-    name: 'имя солдата',
+    name: 'Гилани',
     health: 10,
     weapon: {
         gun: 'AK47',
@@ -8,12 +8,42 @@ const soldier = {
     supplies: 3,
 
     fire: function(){
+        this.weapon.ammunition--
+        console.log('бах-бах')
 
+        if(this.ammunition === 0){
+            console.log('Обойма пуста. Перезаредитесь.')
+        }
     },
     reloading: function(){
-
+        this.weapon.ammunition = 30
+        this.supplies--
+        console.log('Перезарядка...')
+        
+        if(this.supplies === 0){
+            console.log('Не осталось припасов')
+        }
     },
     hurt: function(){
-
+        this.health--
+        if(this.health === 0){
+            console.log('Ты проиграл.')
+        }
     },
-}
+
+    getStatus: function(){
+        console.log(`
+        Имя: ${this.name},
+        Здоровье: ${this.health},
+        Оружие: ${this.weapon.gun},
+        Патроны: ${this.weapon.ammunition}
+        Обоймы: ${this.supplies}.
+        `)
+    },
+};
+
+soldier.hurt()
+soldier.fire()
+soldier.reloading()
+
+soldier.getStatus()
