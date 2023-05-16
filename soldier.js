ae39797ac09d00f4e54ca72b7cceebfa893155f0
BesaMugi <mugiwarabesa@mail.ru>
2023-05-16 21:05:03 +0300

    создал объект "солдат"

---
+++ soldier.js
@@ -0,0 +1,19 @@
+const soldier = {
+    name: 'имя солдата',
+    health: 10,
+    weapon: {
+        gun: 'AK47',
+        ammunition: 30,
+    },
+    supplies: 3,
+
+    fire: function(){
+
+    },
+    reloading: function(){
+
+    },
+    hurt: function(){
+
+    },
+}
