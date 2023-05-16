6ef0f4d5394061f7f4f1b41adbd3ab0cbf9af36e
BesaMugi <mugiwarabesa@mail.ru>
2023-05-16 22:45:19 +0300

    исправил ошибки

--- soldier.js
+++ soldier.js
@@ -3,47 +3,45 @@ const soldier = {
     health: 10,
     weapon: {
         gun: 'AK47',
-        ammunition: 30,
+        ammunition: 3,
     },
     supplies: 3,
 
     fire: function(){
+        if(this.weapon.ammunition === 0){
+            console.log('Обойма пуста. Перезаредитесь.')
+        }else{
         this.weapon.ammunition--
         console.log('бах-бах')
-
-        if(this.ammunition === 0){
-            console.log('Обойма пуста. Перезаредитесь.')
-        }
+    }
     },
     reloading: function(){
-        this.weapon.ammunition = 30
-        this.supplies--
-        console.log('Перезарядка...')
         
         if(this.supplies === 0){
             console.log('Не осталось припасов')
+        }else{
+            this.weapon.ammunition = 3
+            this.supplies--
+            console.log('Перезарядка...')
         }
     },
-    hurt: function(){
-        this.health--
-        if(this.health === 0){
-            console.log('Ты проиграл.')
+        hurt: function(){
+            this.health--
+
+            if(this.health === 0){
+                console.log('Ты проиграл.')
         }
     },
 
     getStatus: function(){
-        console.log(`
-        Имя: ${this.name},
-        Здоровье: ${this.health},
-        Оружие: ${this.weapon.gun},
-        Патроны: ${this.weapon.ammunition}
-        Обоймы: ${this.supplies}.
-        `)
+        console.log(`Имя: ${this.name}, Здоровье: ${this.health}, Оружие: ${this.weapon.gun}, Патроны: ${this.weapon.ammunition}, Обоймы: ${this.supplies}.`)
     },
 };
 
 soldier.hurt()
+
 soldier.fire()
+
 soldier.reloading()
 
 soldier.getStatus()
